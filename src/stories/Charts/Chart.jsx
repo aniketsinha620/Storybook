1216383b5aa5f371e54df608e7842b3f223f6d49
aniketsinha620 <aniketsinha620@gmail.com>
2024-02-03 12:21:33 +0530

    first card

--- src/stories/Charts/Chart.jsx
+++ src/stories/Charts/Chart.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
-
+import Image from 'next/image';
+import pageImage from '../assets/chart.png';
 export default function Chart() {
     return (
         <div>
-            hello
+            <div className="chart">
+                <Image src={pageImage} alt='' width={100} height={100}></Image>
+            </div>
         </div>
     )
 }
--- src/stories/Forms/forms.jsx
+++ src/stories/Forms/forms.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./forms.css"
+import { FaAngleDown } from 'react-icons/fa'; // Importing the dropdown icon
 
 import Input from './Input'
 import { Button } from '../Pages/Button'
@@ -11,15 +12,15 @@ export default function forms() {
                     <h2>XYZ</h2>
                     <p>Welcome Onboard</p>
                     <form>
-                        <Input place="Your Name" icon="true" />
-                        <Input place="Company Name" icon="true" />
-                        <Input place="Select Industry" icon="true" />
+                        <Input place="Your Name" icon=" " />
+                        <Input place="Company Name" icon=" " />
+                        <Input place="Select Industry" icon={FaAngleDown} />
                         <h3 > Select Number of Locations</h3>
                         <div className='form-btn'>
 
                             <Button size="medium" label="1-10" />
                             <Button size="medium" label="10-50" />
-                            <Button size="medium" label="50-150" />
+                            <Button size="black" label="50-150" />
                             <Button size="medium" label="150+" />
                         </div>
 
